1666b64c2f49f9035de42276a485b26862ef2445
OnlineAlex <becasualua@gmail.com>
2018-06-18 16:40:11 +0300

    fix filepath error

--- build.py
+++ build.py
@@ -1,4 +1,5 @@
 import uuid
+import os, glob
 from staticjinja import make_site
 
 
@@ -8,8 +9,12 @@ if __name__ == '__main__':
                     'name': 'Андрей', 'date': 'вчера', 'time': '21:30',
                     'text': '60 шт. ПК от 72-15 до ПК 21-15, Криводановка, с доставкой.', 'views': '12 просмотров'
                     }]*4,
-        'hash': '.{}'.format(uuid.uuid4().hex[:8])  # del 'hash' if you use localhost
+        'hash': '.{}'.format(uuid.uuid4().hex[:8])
     }
+
+    static = glob.glob(os.path.join(os.getcwd(), "static.*"))[0]
+    os.rename(static, 'static{}'.format(context['hash']))
+
     site = make_site(contexts=[('index.html', context), ('leads.html', context)])
     site.render(use_reloader=True)
 
