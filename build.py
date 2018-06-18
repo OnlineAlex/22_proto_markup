bc2da13057364e4274cc83e7c08949f0734188c5
OnlineAlex <becasualua@gmail.com>
2018-06-18 15:02:04 +0300

    add hash to static folder

--- build.py
+++ build.py
@@ -1,10 +1,15 @@
+import uuid
 from staticjinja import make_site
 
-# browser-sync start --server --files "*"
+
 if __name__ == '__main__':
-    context = {'ids': [{'name': 'Андрей', 'date': 'вчера', 'time': '21:30',
-                        'text': '60 шт. ПК от 72-15 до ПК 21-15, Криводановка, с доставкой.', 'views': '12 просмотров'
-                        }]*4}
+    context = {
+        'leads': [{
+                    'name': 'Андрей', 'date': 'вчера', 'time': '21:30',
+                    'text': '60 шт. ПК от 72-15 до ПК 21-15, Криводановка, с доставкой.', 'views': '12 просмотров'
+                    }]*4,
+        'hash': '.{}'.format(uuid.uuid4().hex[:8])  # del 'hash' if you use localhost
+    }
     site = make_site(contexts=[('index.html', context), ('leads.html', context)])
     site.render(use_reloader=True)
 
