669b0d56b854968794b21a69794be8bc1974b1b9
OnlineAlex <becasualua@gmail.com>
2018-06-21 15:19:27 +0300

    add STATIC_URL

--- build.py
+++ build.py
@@ -9,11 +9,12 @@ if __name__ == '__main__':
                     'name': 'Андрей', 'date': 'вчера', 'time': '21:30',
                     'text': '60 шт. ПК от 72-15 до ПК 21-15, Криводановка, с доставкой.', 'views': '12 просмотров'
                     }]*4,
-        'hash': '.{}'.format(uuid.uuid4().hex[:8])
+        'STATIC_URL': 'static.{}'.format(uuid.uuid4().hex[:8])
     }
 
     static = glob.glob(os.path.join(os.getcwd(), "static.*"))[0]
-    os.rename(static, 'static{}'.format(context['hash']))
+    print(static, context['STATIC_URL'])
+    os.rename(static, context['STATIC_URL'])
 
     site = make_site(contexts=[('index.html', context), ('leads.html', context)])
     site.render(use_reloader=True)
